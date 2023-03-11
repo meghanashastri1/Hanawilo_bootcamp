dc6ea7032c1d64c70407b3667614bcc6ba4c0463
meghanashastri1 <meghana360@gmail.com>
2023-03-10 16:32:52 -0800

    finished week2 assignment

---
+++ node_project/controllers/categoryController.js
@@ -0,0 +1,36 @@
+// For '/category' endpoints 
+
+const getCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({message: 'You hit me! Shoe me all the categories'});
+}
+
+const createCategory = (req, res, next) => {
+    res
+    .status(201)
+    .setHeader('Content-Type', 'application/json')
+    .json({message: `Create category with name of ${req.body.categoryName} and gender ${req.body.gender}`});
+}
+
+const putCategory = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({message: 'You hit me! Shoe me all the categories'});
+}
+
+const deleteCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({message: 'Deleting the categories'});
+}
+
+module.exports = {
+    getCategories,
+    createCategory,
+    putCategory,
+    deleteCategories
+};
